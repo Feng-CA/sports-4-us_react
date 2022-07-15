d1b28e387ddcd1494df53c774cecb33d08b50b66
Feng-CA <gcam022106@coderacademy.edu.au>
2022-07-16 00:06:16 +1000

    creates basic home page with image and buttons

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import home from "../assets/home.png"
+import "../styles.css"
+const Home = () => {
+    return(
+        <div className="home"> 
+            <h2>Welcome to Sports 4 Us</h2>
+            <img src={home} alt="landing"/>
+            <button>Sign up</button>
+            <button>Log in</button>
+            <button>Explore activities</button>
+        </div>
+    )
+
+}
+
+export default Home
