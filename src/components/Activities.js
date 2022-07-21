c6f4cb56922db45494180f2db0d164c0bd55d4bc
Anup-SoftwareDev <joseph.anupsa@gmail.com>
2022-07-21 16:11:20 +1000

    full list modified with Tab

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -2,11 +2,13 @@ import Container from '@mui/material/Container';
 import Grid from "@mui/material/Grid";
 import CategoryCard from "./CategoryCard";
 import categories from "../data/categoryList.json";
+import { Link} from "react-router-dom";
+import { Tab } from "@mui/material"
 
 const Activities = () => {
     return (
       <Container>
-        <a href = "/activitylist">Full Actiivity List</a>
+        <Tab label="Full Activity List" component={Link} to="/activitylist"/>
         <Grid container spacing={{ xs: 2, md: 3}} columns={{xs: 4, sm: 8, md: 12}}>
           {categories.map((category, index) => <CategoryCard category={category} key={index} />)}
         </Grid>
