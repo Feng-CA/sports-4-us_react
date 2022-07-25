b47b28de2b4e5cb401a8bd6bdc9c53c431a96cf7
Anup-SoftwareDev <joseph.anupsa@gmail.com>
2022-07-25 12:15:01 +1000

    made changes to Home to display login and sign up buttons only when not logged in

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,18 @@ import { Box, Button, Card, CardMedia, Typography } from "@mui/material"
 import { useNavigate } from "react-router-dom";
 import home from "../assets/home.png";
 import "../style.css"
+import { useGlobalState } from "../utils/stateContext";
 
 const Home = () => {
+    const {store} = useGlobalState()
+    const {loggedInUser} = store
+
     const navigate = useNavigate()
 
     return (
         <Card className="home"> 
             <div style={{position: "relative"}}>
-
+                
                 <Box className="home_heading" style={{position: "absolute", top: 30}}>
                     <Typography className="main_heading" variant="h3">Welcome to Sports 4 Us!</Typography>
                 </Box>
@@ -25,6 +29,8 @@ const Home = () => {
                     </Typography>
                 </Box>
                 <Box sx={{display: "flex"}} style={{position: "absolute", bottom: 20}}>
+                    
+                {!loggedInUser&&
                     <Box sx={{display: "flex"}}> 
                         <Box marginLeft={3}> 
                             <Button variant="contained" onClick={() => navigate("/signup")}>Sign up</Button>
@@ -32,7 +38,7 @@ const Home = () => {
                         <Box marginLeft={2}>
                             <Button variant="contained" color="success" onClick={() => navigate("/login")}>Log in</Button>
                         </Box>
-                    </Box>
+                    </Box>}
                     <Box sx={{display: "flex"}} marginLeft={2} marginRight={2}>
                         <Button variant="text" color="secondary" onClick={() => navigate("/activities")}>Explorer all activities</Button>
                     </Box>
