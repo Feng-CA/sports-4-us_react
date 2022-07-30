88e7da297e953f8fe6f42b6342cb7cbec8493257
Anup-SoftwareDev <joseph.anupsa@gmail.com>
2022-07-31 01:03:49 +1000

    added api services to activities and profiles

--- src/services/activitiesServices.js
+++ src/services/activitiesServices.js
@@ -1,16 +1,25 @@
 import sports4usAPI from "../config/api";
-
+// view full list of activities = no Login required
 export async function getActivities(){
     const response = await sports4usAPI.get('/activities')
     return response     
 }
-
-export async function myActivity(data){
-    const response = await sports4usAPI.get('/activities/myactivities', data)
+// create an Activity => Admin Login required
+export async function createActivity(data){
+    const response = await sports4usAPI.post('/activities', data)
     return response.data
 }
 
-export async function createActivity(data){
-    const response = await sports4usAPI.post('/activities', data)
+// update an Activity => Admin Login required
+//need to provide activity Id and the data to update
+export async function updateActivity(id,data){
+    const response = await sports4usAPI.put(`/activities/${id}`, data)
+    return response.data
+}
+
+// delete an Activity => Admin Login required
+//need to provide activity Id 
+export async function deleteActivity(id){
+    const response = await sports4usAPI.delete(`/activities/${id}`)
     return response.data
 }
