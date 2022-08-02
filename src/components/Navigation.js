08e0cc6b2be4646ce61ba68092274b6373cdbc84
Feng-CA <gcam022106@coderacademy.edu.au>
2022-08-02 20:04:46 +1000

    creates respsonsive navbar with hamburger menu media query

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,20 @@
-import { AppBar, Toolbar, Typography, Tabs, Tab } from "@mui/material"
-import { Link, useNavigate } from "react-router-dom"
-import { useGlobalState } from "../utils/stateContext"
-import { Sports } from '@mui/icons-material';
+import React, { useState } from 'react'
+// import { AppBar, Toolbar, Typography, Tabs, Tab, Box } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+import { useGlobalState } from "../utils/stateContext";
+import { MenuOutlined, Close, Sports } from '@mui/icons-material';
+import "../css/navigation.css";
 
 
 const Navigation = () => {
     const {store, dispatch} = useGlobalState()
     const {loggedInUser} = store
+
+    const [active, setActive] = useState(false)
+
+    const showMenu = () => {
+        setActive(!active)
+    }
     
     const navigate = useNavigate()
 
@@ -24,41 +32,46 @@ const Navigation = () => {
             data: null 
         })
       
-        navigate("/activities")
+        navigate("/categories")
     }
     return (
-        <AppBar position="sticky" color="transparent">
-            <Toolbar>
-                <Sports sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-                <Typography
-                    variant="h6"
-                    noWrap
-                    component="a"
-                    href="/"
-                    sx={{
-                    mr: 2,
-                    display: { xs: 'none', md: 'flex' },
-                    fontFamily: 'monospace',
-                    fontWeight: 700,
-                    letterSpacing: '.3rem',
-                    color: 'inherit',
-                    textDecoration: 'none',
-                    }}
-                >
-                    LOGO
-                </Typography>
-                <Tabs value={false}>
-                    <Tab label="Home" component={Link} to="/" />
-                    <Tab label="Activities" component={Link} to="/categories"/>
-                    { loggedInUser && <Tab label="Member" component={Link} to="/member" />}
-                    <Tab label="Contact" component={Link} to="/contact"/>
-                    { loggedInUser && <Tab label="Logout" onClick={logout} component={Link} to="/activities" />}
-                    { !loggedInUser && <Tab label="Login" component={Link} to="/login" />}      
-                </Tabs>
+        <div className= "navbar">
+            <div className='menu-icon'>
+                {/* <MenuOutlined className="menu" /> */}
+                <MenuOutlined className="menu" onClick={showMenu}/>
+            </div>
+            <nav className={active ? 'slider active' : 'slider'}>
+                <ul className="slider_wrap">
+                    <div className="closed">
+                        {/* <Close className="close"/> */}
+                        <Close className="close" onClick={showMenu}/>
+                    </div>
+                  
+                        <li>
+                            <Link to='/home'>Home</Link>
+                        </li>
+                        <li>
+                            <Link to='/categories'>Activities</Link>
+                        </li>
+                        { loggedInUser && <li>
+                            <Link to='/member'>Member</Link>
+                        </li>}
+                        <li>
+                            <Link to='/contact'>Contact</Link>
+                        </li>
+                       
+                        { loggedInUser &&  <li>
+                            <Link className="purple" onClick={logout} to='/categories'>Logout</Link>
+                        </li>}
+                        { !loggedInUser &&  <li>
+                            <Link className="green" to='/login'>Login</Link>
+                        </li>}      
+            
+                </ul>
                 
-            </Toolbar>
+            </nav>
 
-        </AppBar>
+        </div>
                        
     )
 }
